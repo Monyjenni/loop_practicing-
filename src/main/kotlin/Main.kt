88849b6d09535f5ca85c00013305900f0c09f3e3
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-10-20 15:27:43 +0700

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+
+
+ val values = listOf(1,2,3,4,5)
+    for (numbers in values){
+        println(numbers)
+    }
+
+ val upperLimit = 10
+ for (i in 0 until upperLimit){
+     println(i)
+ }
+
+ val start = 0
+ val stop = 10
+
+ for(i1 in start until stop){
+     println(i1)
+ }
+
+ val myArray = arrayOf(1,2,3)
+
+ for (i2 in myArray){
+     println(i2)
+ }
+
+}
